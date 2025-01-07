99719438dcff39fa593718834ae19a1874b1385d
GNUS <60074926+theMark001@users.noreply.github.com>
2025-01-07 02:48:23 +0100

    refactor: merge funcs in split.py

--- split.py
+++ split.py
@@ -16,37 +16,33 @@ import os
 import math
 from PIL import Image
 
-def split_img(path):
-
-  max_height = 1500
-  img = Image.open(path)
-  img_width, img_height = img.size
-
-  split_count = math.ceil(img_height / max_height)
-  split_height = math.ceil(img_height / split_count)
-
-  base_name, ext = os.path.splitext(os.path.basename(path))
-  base_dir = os.path.dirname(path)
-
-  for i in range(split_count):
-    top = i * split_height
-    bottom = min((i + 1) * split_height, img_height)
-    cropped_img = img.crop((0, top, img_width, bottom))
-
-    split_name = f"{base_name},{i + 1}{ext}"
-    save_path = os.path.join(base_dir, split_name)
-    cropped_img.save(save_path)
-
-  os.remove(path)
-
 def resize_cut(manga_path):
 
   manga_path = os.path.expanduser(manga_path)
-  
+  max_height = 1500
+
   for root, _, files in os.walk(manga_path):
     for file in files:
       file_path = os.path.join(root, file)
       if file.lower().endswith(('.png', '.jpg', '.jpeg', '.img', '.tiff', '.bmp', '.gif')):
-        split_img(file_path)
+        img = Image.open(file_path)
+        img_width, img_height = img.size
+
+        split_count = math.ceil(img_height / max_height)
+        split_height = math.ceil(img_height / split_count)
+
+        base_name, ext = os.path.splitext(os.path.basename(file_path))
+        base_dir = os.path.dirname(file_path)
+
+        for i in range(split_count):
+          top = i * split_height
+          bottom = min((i + 1) * split_height, img_height)
+          cropped_img = img.crop((0, top, img_width, bottom))
+
+          split_name = f"{base_name},{i + 1}{ext}"
+          save_path = os.path.join(base_dir, split_name)
+          cropped_img.save(save_path)
+
+        os.remove(file_path)
 
 resize_cut("~/Desktop/Projects/manga2books/Childhood Friend Of The Zenith")
