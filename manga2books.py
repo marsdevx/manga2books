4deb62268c266d17098c966f5b5a9f9b1ebb2103
GNUS <60074926+theMark001@users.noreply.github.com>
2025-01-07 05:37:35 +0100

    feat: chose chapters

--- manga2books.py
+++ manga2books.py
@@ -20,14 +20,15 @@ import download
 import get_cover
 
 def main():
-  if len(sys.argv) < 2:
+  if len(sys.argv) != 2 and len(sys.argv) != 3:
     print("Usage: manga2books <manga_url_manga>")
     sys.exit(1)
 
   url = sys.argv[1]
+  ch_range = sys.argv[2] if len(sys.argv) > 2 else None
 
   print("Downloading imgs")
-  name = download.download_extract(url)
+  name = download.download_extract(url, ch_range)
 
   print("Downloading cover img for Manga")
   get_cover.get_cover(url)
